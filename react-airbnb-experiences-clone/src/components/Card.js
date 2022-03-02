313f843fb674bc2760bb68508a029df41b391d81
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2022-03-02 23:06:39 +0530

    object spread

--- react-airbnb-experiences-clone/src/components/Card.js
+++ react-airbnb-experiences-clone/src/components/Card.js
@@ -1,12 +1,12 @@
 import star from '../star.png'
 
-function Card({img, rating, reviewCount, title, price, country, openSpots}) {
+function Card({coverImg, rating, reviewCount, title, price, location, openSpots}) {
     let badgeText
     if(openSpots===0)
     {
         badgeText="SOLD OUT"
     }
-    else if(country==="Online")
+    else if(location==="Online")
     {
         badgeText="ONLINE"
     }
@@ -16,13 +16,13 @@ function Card({img, rating, reviewCount, title, price, country, openSpots}) {
                 {badgeText && <div className="badge">{badgeText}</div>}
                 
                 <div className="card-image-container">
-                    <img src={`./images/${img}`} alt="katie" className='card-image'/>
+                    <img src={`./images/${coverImg}`} alt="katie" className='card-image'/>
                 </div>
                 <div className="card-content">
                     <div className="rating">
                         <img src={star} alt="star" className='star'/>
                         <p className="rating">{rating} </p>
-                        <p className="review-count"> ({reviewCount}) | {country}</p>
+                        <p className="review-count"> ({reviewCount}) | {location}</p>
                     </div>
                     <p className='card-text'>
                         {title}
